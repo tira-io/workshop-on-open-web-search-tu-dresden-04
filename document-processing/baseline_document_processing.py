2e881c0814e971e219f33db78d989fb431cd111f
maria-hampel <92380493+maria-hampel@users.noreply.github.com>
2024-03-04 18:30:50 +0000

    fixed stuff

--- document-processing/baseline_document_processing.py
+++ document-processing/baseline_document_processing.py
@@ -7,47 +7,42 @@ import textstat as ts
 from pprint import pprint 
 import matplotlib.pyplot as plt
 import json
-from datetime import datetime
+import gzip
 
-fake_now=datetime.now()
-now=fake_now.isoformat()
-
-doc_ranking=dict()
 
 def process_document(document):
     # Dummy processing of documents: classify each document as spam
-    doc_ranking[document.doc_id]={'flesch_reading_formula': ts.flesch_reading_ease(document.text), 
-                                  'flesch_kincaid_grade': ts.flesch_kincaid_grade(document.text), 
-                                  'gunning_fog': ts.gunning_fog(document.text),
-                                  'automated_readability_index': ts.automated_readability_index(document.text),
-                                  'coleman_liau_index': ts.coleman_liau_index(document.text),
-                                  'linsear_write_formula': ts.linsear_write_formula(document.text),
-                                  'dale_chall_readability_score': ts.dale_chall_readability_score(document.text),
-                                  'text_standard': ts.text_standard(document.text, float_output=True),
-                                  'spache_readability': ts.spache_readability(document.text),
-                                  'mcalpine_eflaw': ts.mcalpine_eflaw(document.text)}
-    return {'docno': document.doc_id, 'label': 'spam'}
+    return {'docno': document.doc_id, 
+            'flesch_reading_formula': ts.flesch_reading_ease(document.text), 
+            'flesch_kincaid_grade': ts.flesch_kincaid_grade(document.text), 
+            'gunning_fog': ts.gunning_fog(document.text),
+            'automated_readability_index': ts.automated_readability_index(document.text),
+            'coleman_liau_index': ts.coleman_liau_index(document.text),
+            'linsear_write_formula': ts.linsear_write_formula(document.text),
+            'dale_chall_readability_score': ts.dale_chall_readability_score(document.text),
+            'text_standard': ts.text_standard(document.text, float_output=True),
+            'spache_readability': ts.spache_readability(document.text),
+            'mcalpine_eflaw': ts.mcalpine_eflaw(document.text)}
 
 
 def process_documents(document_iter):
     return pd.DataFrame([process_document(i) for i in document_iter])
 
 # convert the dictionary to a Dataframe, so we can visualize the result as a bar graph later
-def convert_result_dict_to_df():
-    df = pd.DataFrame.from_dict(doc_ranking, orient='index')
-    print(df)
+def convert_result_dict_to_df(output_file):
+    df = pd.DataFrame.from_dict(output_file, orient='index')
     return df
 
 # plot the data in an easy bar plot
 def plot_data_easy(df):
     fig = df.plot(kind='bar',  
         figsize=(20, 16), fontsize=26).get_figure()
-    fig.savefig(now+'__test.pdf')
+    fig.savefig('test.pdf')
     
 # Create result.json 
 def create_result_json():
-    with open(now+"__result.json", "w") as write_file:
-        json.dump(doc_ranking, write_file, indent=4, separators=(", ", ": "), sort_keys=True)
+    with open("result.json", "w") as write_file:
+        json.dump(output_file, write_file, indent=4, separators=(", ", ": "), sort_keys=True)
 
 
 if __name__ == '__main__':
@@ -65,7 +60,6 @@ if __name__ == '__main__':
     # process the documents, store results at expected location.
     processed_documents = process_documents(dataset.docs_iter())
     processed_documents.to_json(output_file, lines=True, orient='records')
-    pprint(doc_ranking)
-    df = convert_result_dict_to_df() 
-    plot_data_easy(df)  
-    create_result_json()    
+    #df = convert_result_dict_to_df(data) 
+    #plot_data_easy(df)  
+    #create_result_json()    
