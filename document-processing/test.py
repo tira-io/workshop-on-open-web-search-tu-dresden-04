d578315afd6a1377cc148eb875ae4bc3ec180e5f
Linda Erben <73427590+Arvlis@users.noreply.github.com>
2024-03-04 15:02:04 +0000

    test docs found

---
+++ document-processing/test.py
@@ -0,0 +1,12 @@
+
+# here are some sample documents
+
+t_doc_simple_language = "Several bird species make and use tools. Some social species pass on some knowledge across generation. This is a form of culture. Birds are social animals."
+t_doc_academic_paper = "This paper investigates the channel capacity of Internet-based timing channels and proposes a methodology for detecting covert timing channels based on how close a source comes to achieving that channel capacity. A statistical approach is then used for the special case of binary codes."
+t_doc_school_physics = "What is the definition of a wave? The wave meaning is that a wave is an oscillating, regular distortion through spacetime. Some waves, called electromagnetic waves, can travel through a vacuum, or a space void of matter. Some waves, called mechanical waves, require a medium to propagate."
+t_doc_kids = "Onions might taste good but they can be painful to chop. A gas is released when you cut onions that irritates you eyes, the tears you produce while this happens are your body’s way of washing it from your eyes."
+t_doc_inappropriate_for_kids = "bitch, fuck, "
+t_doc_colloquial = "y u always mad????"
+
+def test01():
+    return 0
