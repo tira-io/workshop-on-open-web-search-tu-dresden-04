4f3eb0139995029043c5fe7f9b8d398e8b311999
Linda Erben <73427590+Arvlis@users.noreply.github.com>
2024-03-07 15:14:55 +0000

    spacy use vector module

--- spacy-document-processing/baseline_document_processing.py
+++ spacy-document-processing/baseline_document_processing.py
@@ -11,7 +11,7 @@ import json
 import gzip
 from tqdm import tqdm
 
-nlp=spacy.load("en_core_web_sm")
+nlp=spacy.load("en_core_web_md")
 
 def process_dataset(document_iter):
     result=pd.DataFrame([{'docno': i.doc_id} for i in tqdm(document_iter)])
@@ -19,7 +19,7 @@ def process_dataset(document_iter):
 
 
 def process_metrics(document_iter):
-    nlp.add_pipe("textdescriptives/readability", "textdescriptives/descriptive_stats")
+    nlp.add_pipe("textdescriptives/all")
     docs = nlp.pipe([doc.text[:5000] for doc in tqdm(document_iter)])
     metrics = td.extract_df(docs, include_text = False)
     return metrics
@@ -46,6 +46,6 @@ if __name__ == '__main__':
     processed_dataset=process_dataset(dataset.docs_iter())
     processed_metrics = process_metrics(dataset.docs_iter())
     processed_documents = pd.concat([processed_dataset, processed_metrics], axis=1)
-    plot_data_easy(processed_documents)
+    # plot_data_easy(processed_documents)
     processed_documents.to_json(output_file, lines=True, orient='records')
     
